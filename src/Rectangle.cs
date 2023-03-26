5e5ac86153a00ae86ceda8bac14f4a36638c5c12
ASlightlyOvergrownCactus <ASlightlyOvergrownCactus@gmail.com>
2023-03-26 13:11:05 -0500

    Rotation!!!
    Added rotation to the square, next up is adding rotational velocity to it. (and then we get to properly adding physics instead of lazily doing them).

--- src/Rectangle.cs
+++ src/Rectangle.cs
@@ -4,29 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using System.Drawing.Drawing2D;
 
 namespace TestMod
 {
     class Rectangle
     {
-        public Vector2 center;
-        public Vector2[] corners;
+        public UnityEngine.Vector2 center;
+        public UnityEngine.Vector2[] corners;
         public float width;
         public float height;
         public float angleRad;
 
-        public Rectangle(Vector2 center, float width, float height)
+        public Rectangle(UnityEngine.Vector2 center, float width, float height)
         {
             this.center = center;
             this.width = width;
             this.height = height;
-            corners = new Vector2[4];
+            corners = new UnityEngine.Vector2[4];
             UpdateCornerPointsWithAngle(90f);
         }
 
         
 
-        public void Move(Vector2 velocity)
+        public void Move(UnityEngine.Vector2 velocity)
         {
             center += velocity * Time.deltaTime;
         }
@@ -35,58 +36,36 @@ namespace TestMod
         {
             //Debug.Log(center);
             // Define the corner points of the rectangle
-            corners[0] = new Vector2(center.x - width / 2f, center.y - height / 2f); // Bottom Left
-            corners[1] = new Vector2(center.x + width / 2f, center.y - height / 2f); // Bottom Right
-            corners[2] = new Vector2(center.x + width / 2f, center.y + height / 2f); // Top Left
-            corners[3] = new Vector2(center.x - width / 2f, center.y + height / 2f); // Top Right
+            corners[0] = new UnityEngine.Vector2(0 - width / 2f, 0 - height / 2f); // Bottom Left
+            corners[1] = new UnityEngine.Vector2(0 + width / 2f, 0 - height / 2f); // Bottom Right
+            corners[2] = new UnityEngine.Vector2(0 + width / 2f, 0 + height / 2f); // Top Left
+            corners[3] = new UnityEngine.Vector2(0 - width / 2f, 0 + height / 2f); // Top Right
 
-            // Define the rotation matrix (Doesnt work right now)
-            //Matrix4x4 rotationMatrix = new();
-            //rotationMatrix.SetColumn(0, new Vector4(Mathf.Cos(angleRad), -Mathf.Sin(angleRad), 0f, 0f));
-            //rotationMatrix.SetColumn(1, new Vector4(Mathf.Sin(angleRad), Mathf.Cos(angleRad), 0f, 0f));
-            //rotationMatrix.SetColumn(2, new Vector4(0f, 0f, 1f, 0f));
-            //rotationMatrix.SetColumn(3, new Vector4(0f, 0f, 0f, 1f));
-
-            // Apply the rotation matrix to each corner point
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    corners[i] = rotationMatrix * new Vector4(corners[i].x, corners[i].y, 0f, 1f);
-            //}
+            angleRad = 45f * Mathf.Deg2Rad;
+            // Loop through each corner point
+            for (int i = 0; i < corners.Length; i++)
+            {
+                corners[i] = RWCustom.Custom.RotateAroundOrigo(corners[i], 45f);
+                corners[i] += center;
+            }
         }
 
-        public void UpdateCornerPointsWithAngle(float angle)
+        public void UpdateCornerPointsWithAngle(float angleAdded)
         {
             // Define the corner points of the rectangle
             //Debug.Log(center);
-            corners[0] = new Vector2(center.x - width / 2f, center.y - height / 2f); // Bottom Left
-            
-            corners[1] = new Vector2(center.x + width / 2f, center.y - height / 2f); // Bottom Right
-            
-            corners[2] = new Vector2(center.x - width / 2f, center.y + height / 2f); // Top Left
-            
-            corners[3] = new Vector2(center.x + width / 2f, center.y + height / 2f); // Top Right
-            
+            corners[0] = new UnityEngine.Vector2(0 - width / 2f, 0 - height / 2f); // Bottom Left
+            corners[1] = new UnityEngine.Vector2(0 + width / 2f, 0 - height / 2f); // Bottom Right
+            corners[2] = new UnityEngine.Vector2(0 + width / 2f, 0 + height / 2f); // Top Left
+            corners[3] = new UnityEngine.Vector2(0 - width / 2f, 0 + height / 2f); // Top Right
 
-            // Convert the rotation angle from degrees to radians
-            angleRad = angle * Mathf.Deg2Rad;
+            // Loop through each corner point
+            for (int i = 0; i < corners.Length; i++)
+            {
+                corners[i] = RWCustom.Custom.RotateAroundOrigo(corners[i], 45f);
+                corners[i] += center;
+            }
 
-            // Define the rotation matrix (doesnt work right now)
-            Matrix4x4 rotationMatrix = new();
-            rotationMatrix.SetColumn(0, new Vector4(Mathf.Cos(angleRad), -Mathf.Sin(angleRad), 0f, 0f));
-            rotationMatrix.SetColumn(1, new Vector4(Mathf.Sin(angleRad), Mathf.Cos(angleRad), 0f, 0f));
-            rotationMatrix.SetColumn(2, new Vector4(0f, 0f, 1f, 0f));
-            rotationMatrix.SetColumn(3, new Vector4(0f, 0f, 0f, 1f));
-
-            // Apply the rotation matrix to each corner point
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    corners[i] = rotationMatrix * new Vector4(corners[i].x, corners[i].y, 0f, 1f);
-            //}
-
-            //Debug.Log(corners[0]);
-            //Debug.Log(corners[1]);
-            //Debug.Log(corners[2]);
-            //Debug.Log(corners[3]);
         }
     }
 }
